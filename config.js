59055b79f129a1770e25ff600bd3ed6a9d9cf860
Jussi Rytkönen <jussi.rytkonen@gmail.com>
2015-11-16 17:14:14 +0200

    jspm package cleanup

--- config.js
+++ config.js
@@ -15,46 +15,11 @@ System.config({
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.8.33",
-    "babel-runtime": "npm:babel-runtime@5.8.29",
     "coffee": "github:forresto/system-coffee@0.1.2",
-    "core-js": "npm:core-js@0.9.18",
-    "jussiry/jsonHtml": "github:jussiry/jsonHtml@0.1.0",
-    "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.3.0"
-    },
-    "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.2"
-    },
-    "github:jspm/nodelibs-util@0.1.0": {
-      "util": "npm:util@0.10.3"
-    },
+    "jsonCss": "github:jussiry/jsonCss@master",
+    "jsonHtml": "github:jussiry/jsonHtml@0.1.0",
     "github:jussiry/jsonHtml@0.1.0": {
       "coffee": "github:forresto/system-coffee@0.1.2"
-    },
-    "github:jussiry/jsonHtml@master": {
-      "coffee": "github:forresto/system-coffee@0.1.2"
-    },
-    "npm:assert@1.3.0": {
-      "util": "npm:util@0.10.3"
-    },
-    "npm:babel-runtime@5.8.29": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:core-js@0.9.18": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:inherits@2.0.1": {
-      "util": "github:jspm/nodelibs-util@0.1.0"
-    },
-    "npm:process@0.11.2": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0"
-    },
-    "npm:util@0.10.3": {
-      "inherits": "npm:inherits@2.0.1",
-      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
--- index.coffee
+++ index.coffee
@@ -1,12 +1,59 @@
 
-{ parse } = require "jussiry/jsonHtml"
+{ parse } = require "jsonHtml"
 
+{ addStyles, compile, removeStyles } = require "jsonCss"
+
+styleId = 'some-style-id'
+
+# HTML
 
 htmlObj =
-  '.main':
-    button: 'click!'
+  '.json-html-style-example':
+    h1: "Some html to target the styles"
+    'h1.second': ".second title"
+    ul: [
+      li: "dii"
+    , li: "daa"
+    ]
+    'button.remove':
+      text: 'remove style definitions'
+      onClick: -> removeStyles styleId
+
+# STYLES
+
+blackBox =
+  padding: 10
+  background: 'black'
+  color: 'white'
+
+cssDefinitions =
+  '.json-html-style-example':
+    # camelCase poperty names transform to kebab-case, e.g.
+    # borderRadius -> border-radius
+    borderRadius: "10px"
+    # pixel values can be defined with plain numbers:
+    padding: 10
+    background: 'white'
+    color: 'black'
+    h1:
+      fontSize: 14
+      # & -sign can be used to mixin definition to parent
+      '&': blackBox
+      # unlike with jsonHtml, whitespace characters do matter
+      '&.second':
+        color: "hsl(120, 90%, 45%)"
     ul:
-      for i in [1,2,3]
-        li: i
+      padding: blackBox.padding
+      li:
+        # add -v to property to create vendor prefixes
+        "filter -v": "blur(2px)"
+    '.remove':
+      color: 'red'
+      margin: 10
+  # nested definitions can also be define in one string
+  'body pre': blackBox
+
+# INIT
 
 parse htmlObj, document.querySelector('#root')
+addStyles styleId, cssDefinitions
